22d283d27e1e13edcac34bdbcbaa7bbf679c4679
Emmanuel Evberin <emmathedeveloper@gmail.com>
2024-01-17 00:23:16 +0100

    minor patches

---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+    function handleSubmit(e: any){
+
+        const data = new FormData(e.target)
+
+        data.forEach((v , k) => console.log(k , v))
+    }
+</script>
+
+<section>
+    <form on:submit|preventDefault={handleSubmit}>
+        <label for="pdf_file_input">Upload PDF or DOCX file</label>
+        <input name="pdf_file_input" type="file"/>
+        <input type="text" placeholder="title" />
+        <input type="text" placeholder="description" />
+        <button>Upload</button>
+    </form>
+</section>
